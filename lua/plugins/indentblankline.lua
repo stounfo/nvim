b41c08aaac1880a56a1360106a850be8dd37ab9f
Oleg Levantin <stounfo@gmail.com>
2023-11-01 13:14:10 +0400

    Bump indentblankline

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,8 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    show_current_context = true,
-  },
+  main = "ibl",
   config = true,
 }
