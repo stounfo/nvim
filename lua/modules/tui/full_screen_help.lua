f7cb6d676d0db4df04e600379c4d56afda9d6742
Oleg Levantin <stounfo@gmail.com>
2024-05-20 14:45:33 +0400

    Fix full_screen_help

--- lua/modules/tui/full_screen_help.lua
+++ lua/modules/tui/full_screen_help.lua
@@ -1,6 +1,7 @@
 -- open help files in full window
 local _ = {
-    vim.api.nvim_create_autocmd("BufEnter", {
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "help",
         callback = function()
             if vim.bo.filetype == "help" then
                 vim.cmd("only")
