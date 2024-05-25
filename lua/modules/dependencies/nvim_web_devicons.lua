38aba2ecd48033df178957635ed02010b30f1735
Oleg Levantin <stounfo@gmail.com>
2024-05-25 13:11:36 +0400

    Up dev icons and syntax highlighting

--- lua/modules/dependencies/nvim_web_devicons.lua
+++ lua/modules/dependencies/nvim_web_devicons.lua
@@ -1,7 +1,16 @@
+local color = require("colors").dark_foreground
+
 return {
     "nvim-tree/nvim-web-devicons",
     config = function()
         local devicons = require("nvim-web-devicons")
-        devicons.setup()
+        local colorized_icons = {}
+        for key, value in pairs(devicons.get_icons()) do
+            value.cterm_color = color
+            table.insert(colorized_icons, { key, value })
+        end
+        devicons.setup({
+            override = colorized_icons,
+        })
     end,
 }
