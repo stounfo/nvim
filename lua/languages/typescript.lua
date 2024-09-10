d008cbde9811a118ab81f267244c227891d45edb
Oleg Levantin <stounfo@gmail.com>
2024-09-10 23:50:55 +0400

    Update

--- lua/languages/typescript.lua
+++ lua/languages/typescript.lua
@@ -3,7 +3,7 @@ local M = {}
 M.mason_to_install = { "typescript-language-server", "prettier", "eslint-lsp" }
 M.treesitter_to_install = { "typescript", "tsx" }
 M.lsp_configs = {
-    tsserver = {},
+    ts_ls = {},
     eslint = {},
 }
 M.formatters = {
