c0334b717b26d428e3964a17012b50a2123a9149
Oleg Levantin <stounfo@gmail.com>
2024-12-15 23:19:41 +0400

    feat(editor): replace nvim-cmp with blink.cmp
    - Removed nvim-cmp and its dependencies from the project.
    - Added blink.cmp with new configuration and dependencies.
    - Updated lazy-lock.json to reflect the changes.

---
+++ lua/modules/editor/completion/blink.lua
@@ -0,0 +1,88 @@
+local dependencies = {
+    "rafamadriz/friendly-snippets",
+}
+
+local colorscheme = {
+    BlinkCmpMenu = {
+        link = "NormalFloat",
+    },
+    BlinkCmpMenuBorder = {
+        link = "FloatBorder",
+    },
+    BlinkCmpMenuSelection = {
+        link = "CursorLine",
+    },
+    BlinkCmpDocBorder = {
+        link = "BlinkCmpMenuBorder",
+    },
+    BlinkCmpSignatureHelpBorder = {
+        link = "BlinkCmpMenuBorder",
+    },
+}
+
+local options = function()
+    return {
+        keymap = {
+            ["<CR>"] = { "accept", "fallback" },
+            ["<Tab>"] = { "accept", "fallback" },
+
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+
+            -- ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+            -- ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        },
+        sources = {
+            default = { "lsp", "snippets", "buffer", "path" },
+        },
+        completion = {
+            accept = {
+                auto_brackets = {
+                    enabled = true,
+                },
+            },
+            documentation = {
+                winblend = 10,
+                auto_show = true,
+                auto_show_delay_ms = 200,
+                window = {
+                    border = "rounded",
+                },
+            },
+            menu = {
+                draw = {
+                    columns = {
+                        { "label", "label_description", gap = 1 },
+                        { "kind" },
+                    },
+                },
+                enabled = true,
+                border = "rounded",
+            },
+        },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+
+        signature = {
+            enabled = true,
+            window = {
+                border = "rounded",
+            },
+        },
+    }
+end
+
+return {
+    "saghen/blink.cmp",
+    version = "v0.*",
+    event = "InsertEnter",
+    dependencies = dependencies,
+    opts = options,
+    config = function(_, opts)
+        require("utils").set_hl(colorscheme)
+        require("blink.cmp").setup(opts)
+    end,
+}
--- lua/modules/editor/completion/init.lua
+++ lua/modules/editor/completion/init.lua
@@ -1,5 +1,5 @@
 return {
     require("modules.editor.completion.autoparis"),
     require("modules.editor.completion.copilot"),
-    require("modules.editor.completion.nvim_cmp"),
+    require("modules.editor.completion.blink"),
 }
