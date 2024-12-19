9f808850ea058a76d428ec40f46b7e7ca3d76170
Oleg Levantin <stounfo@gmail.com>
2024-12-19 13:10:10 +0400

    fix(dependencies): convert color to string in devicons

--- lua/modules/dependencies/nvim_web_devicons.lua
+++ lua/modules/dependencies/nvim_web_devicons.lua
@@ -6,7 +6,7 @@ return {
         local devicons = require("nvim-web-devicons")
         local colorized_icons = {}
         for key, value in pairs(devicons.get_icons()) do
-            value.cterm_color = color
+            value.cterm_color = tostring(color)
             table.insert(colorized_icons, { key, value })
         end
         devicons.setup({
